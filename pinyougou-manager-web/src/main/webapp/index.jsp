9ea8e79e8cb91238c684ad35544f0bf995422318
wfq <377574804@qq.com>
2018-12-13 19:33:46 +0800

    商品管理-品牌管理 完成分页查询，条件查询，增删改功能

--- pinyougou-manager-web/src/main/webapp/index.jsp
+++ pinyougou-manager-web/src/main/webapp/index.jsp
@@ -2,4 +2,5 @@
 <body>
 <h2>Hello World!</h2>
 </body>
+<%--<jsp:forward page="/admin"></jsp:forward>--%>
 </html>
--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/BrandService.java
@@ -1,6 +1,7 @@
 package com.pinyougou.sellergoods;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
@@ -10,4 +11,47 @@ public interface BrandService {
      * @return
      */
     public List<TbBrand> findAll();
+
+    /**
+     * 品牌分页
+     * @param pageNum 当前页面
+     * @param pageSize 每页记录数
+     * @return
+     */
+    public PageResult findPage(int pageNum, int pageSize);
+
+    /**
+     * 增加
+     * @param brand
+     */
+    public void add(TbBrand brand);
+
+    /**
+     * 根据ID查询实体
+     * @param id
+     * @return
+     */
+    public TbBrand findOne(Long id);
+
+    /**
+     * 修改
+     * @param brand
+     */
+    public void update(TbBrand brand);
+
+
+    /**
+     * 删除
+     * @param ids
+     */
+    public void delete(Long[] ids);
+
+
+    /**
+     * 品牌分页
+     * @param pageNum 当前页面
+     * @param pageSize 每页记录数
+     * @return
+     */
+    public PageResult findPage1(TbBrand brand, int pageNum,int pageSize);
 }
